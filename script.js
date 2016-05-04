08c087466805df07411ac934a84daabbfd240fff
Nguyen Ngoc Sang <1312485@student.hcmus.edu.vn>
2016-05-04 15:30:50 +0700

    Sữa input type="text" ở những chỗ dùng ngày  thành input "date" kiểm tra xem dữ liệu nhập vào có phải ngày không. Trong file scripts gán kiểu các chuỗi trong thành kiểu ngày Sữa lỗi gán các giá trị bằng null khi thêm mới. Thêm ảnh vô để xóa các skill Trong file css tạo hình bút chỉnh sữa

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ var module = angular.module('App', []);
 module.controller('firstController', function($scope, $http) {
 	$http.get('json.json').success(function(data) {
 		$scope.mydata = data;
+		$scope.mydata.birthdate = new Date($scope.mydata.birthdate);
+		for(let i = 0 ; i < $scope.mydata.experiences.length; i++ )
+		{
+			$scope.mydata.experiences[i].time = new Date($scope.mydata.experiences[i].time);
+
+		}
+		for(let i = 0 ; i < $scope.mydata.projects.length; i++ )
+		{
+			$scope.mydata.projects[i].time = new Date($scope.mydata.projects[i].time);
+		}
+		for(let i = 0 ; i < $scope.mydata.educations.length; i++ )
+		{
+			$scope.mydata.educations[i].graduate = new Date($scope.mydata.educations[i].graduate);
+		}
 	});
 	$scope.displayname = false;
 	$scope.displayjob = false;
@@ -11,7 +25,9 @@ module.controller('firstController', function($scope, $http) {
 	$scope.displaysummary =false;
 	$scope.displayexperene = false;
 	$scope.displayproject = false;
-	$scope.displayeducation = false; 
+	$scope.displayeducation = false;
+	$scope.displayskill = false;
+	$scope.displayotherskill = false;
 	$scope.editname = function (){
 		$scope.edit_name = $scope.mydata.name;
 		$scope.displayname = true;
@@ -69,6 +85,13 @@ module.controller('firstController', function($scope, $http) {
 			$scope.edit_time = $scope.mydata.experiences[index].time;
 			$scope.edit_describe = $scope.mydata.experiences[index].Describe;
 		}
+		else{
+			$scope.edit_company = "";
+			$scope.edit_location = "";
+			$scope.edit_time = "";
+			$scope.edit_describe = "";
+			$scope.edit_logo = "";
+		}
 		$scope.displayexperene = true;
 	}
 	$scope.saveexperene = function(index){
@@ -99,6 +122,12 @@ module.controller('firstController', function($scope, $http) {
 			$scope.edit_timeproject = $scope.mydata.projects[index].time;
 			$scope.edit_describeproject = $scope.mydata.projects[index].Describe;
 		}
+		else{
+			$scope.edit_project = "";
+			$scope.edit_logoproject = "";
+			$scope.edit_timeproject = "";
+			$scope.edit_describeproject = "";
+		}
 		$scope.displayproject = true;
 	}
 	$scope.saveproject = function(index){
@@ -107,8 +136,8 @@ module.controller('firstController', function($scope, $http) {
 			$scope.mydata.projects.push({name:$scope.edit_project,Logo:$scope.edit_logoproject,time:$scope.edit_timeproject,Describe:$scope.edit_describeproject});
 			$scope.edit_project = "";
 			$scope.edit_logoproject = "";
-			$scope.edit_time = "";
-			$scope.edit_describe = "";
+			$scope.edit_timeproject = "";
+			$scope.edit_describeproject = "";
 		}
 		else{
 			$scope.mydata.projects[index].name = $scope.edit_project;
@@ -128,16 +157,24 @@ module.controller('firstController', function($scope, $http) {
 			$scope.edit_timeeducation = $scope.mydata.educations[index].graduate;
 			$scope.edit_describeeducation = $scope.mydata.educations[index].Describe;
 		}
+		else{
+			$scope.edit_education = "";
+			$scope.edit_major = "";
+			$scope.edit_logoeducation = "";
+			$scope.edit_timeeducation = "";
+			$scope.edit_describeeducation = "";
+		}
 		$scope.displayeducation = true;
 	}
 	$scope.saveeducation = function(index){
 		if(index == -1)
 		{
 			$scope.mydata.educations.push({name:$scope.edit_education,major:$scope.edit_major,graduate:$scope.edit_timeeducation,logo:$scope.edit_logoeducation,Describe:$scope.edit_describeeducation});
-			$scope.edit_project = "";
-			$scope.edit_logoproject = "";
-			$scope.edit_time = "";
-			$scope.edit_describe = "";
+			$scope.edit_education = "";
+			$scope.edit_major = "";
+			$scope.edit_logoeducation = "";
+			$scope.edit_timeeducation = "";
+			$scope.edit_describeeducation = "";
 		}
 		else{
 			$scope.mydata.educations[index].name = $scope.edit_education;
@@ -147,5 +184,25 @@ module.controller('firstController', function($scope, $http) {
 			$scope.mydata.educations[index].Describe = $scope.edit_describeeducation;
 		}
 		$scope.displayeducation = false;
+	}
+	$scope.editskill = function (){
+		$scope.displayskill = true;
+	}
+	$scope.removeskill = function(index){
+		$scope.mydata.skills.splice(index,1);
+	}
+	$scope.saveskill = function(){
+		$scope.mydata.skills.push($scope.edit_skill);
+		$scope.displayskill = false;
+	}
+	$scope.editotherskill = function (){
+		$scope.displayotherskill = true;
+	}
+	$scope.removeotherskill = function(index){
+		$scope.mydata.differentskills.splice(index,1);
+	}
+	$scope.saveotherskill = function(){
+		$scope.mydata.differentskills.push($scope.edit_otherskill);
+		$scope.displayotherskill = false;
 	}
 });
